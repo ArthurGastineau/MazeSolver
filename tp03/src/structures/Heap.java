b83de87c05b049b8eecea1454a2860107b987cd1
Arthur Gastineau <arthur.gastineau@telecom-paris.fr>
2022-10-03 19:59:08 +0200

    TP05 - pullUp(int) & buildHeap()

--- tp03/src/structures/Heap.java
+++ tp03/src/structures/Heap.java
@@ -9,4 +9,29 @@ public class Heap {
 	public Heap(String[] args) {
 		heap = args.clone();
 	}
+	
+	public void swap (int i, int j) {
+		String temp  = heap[i];
+		heap[i] = heap[j];
+		heap[j] = temp;
+	}
+	
+	private void pullUp (int k) {
+		int son = k - 1;
+		int father = (k - 1) / 2;
+		
+		while ( (son > 0) && (heap[father].compareTo(heap[son]) < 0) ) {		
+				swap(son, father);
+				son = father;
+				father = (son - 1) / 2;		
+		}
+	}
+	
+	private void buildHeap() {
+		int sizeof = heap.length;
+		for (int i = 2; i < sizeof; i++) {
+			pullUp(i);
+		}
+	}
+	
 }
