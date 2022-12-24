7359f8aac21e5dd11536b165f05072ff075b27df
arthur <arthur@localhost>
2022-12-24 16:25:13 +0100

    Perfect hexagonal maze

--- tp04/src/graphics/HexagonalTable.java
+++ tp04/src/graphics/HexagonalTable.java
@@ -1,6 +1,7 @@
 package graphics;
 
 import java.awt.Color;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
@@ -11,7 +12,7 @@ public class HexagonalTable {
   private static final int ORIGIN_X = 100;
   private static final int ORIGIN_Y = 100;
   private static final double SQRT_3 = Math.sqrt(3);
-  private static final int SIZE =25;
+  private static final int size =30;
   private static final Color[] COLORS = {Color.RED, Color.YELLOW, Color.CYAN, Color.GREEN};
   
   private static final int numberOfBoxes = 10;
@@ -21,7 +22,7 @@ public class HexagonalTable {
     HexagonalTablePanel panel = new HexagonalTablePanel();
     frame.add(panel);
     frame.addMouseMotionListener(panel);
-    frame.setSize(600,600);
+    frame.setSize(800,800);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
   }
@@ -33,59 +34,63 @@ private static class HexagonalTablePanel extends JPanel implements MouseMotionLi
 
     @Override
     public void paintComponent(Graphics g) {
-    		int x = 0;
-    		int y = 0;
-    		String text = "";
-           for (int i = 0; i < numberOfBoxes; i++) {
+		int x = 0;
+		int y = 0;
+		String text = "";
+       for (int i = 0; i < numberOfBoxes; i++) {
 
-        	   for (int j= 0; j < numberOfBoxes; j++) {
-        		   Color c = COLORS[j % 2 + 2 * (i % 2)];
-        		   y= ORIGIN_Y + (int) ((1.5 * SIZE * i) );
-        		   x = ORIGIN_X + (int) ((j * SQRT_3 * SIZE) + ((i%2) * SIZE * SQRT_3 / 2));
-        		   if (i == selectedRow && j == selectedColumn) {
-        	            drawHex(g, x, y, SIZE, Color.BLACK, text);
-        	          } else {
-        	            drawHex(g, x, y, SIZE, c, text);
-        	          }
-        	   }
-           }
-         
-    }
+    	   for (int j= 0; j < numberOfBoxes; j++) {
+    		   Color c = COLORS[j % 2 + 2 * (i % 2)];
+    		   y= ORIGIN_Y + (int) ((1.5 * size * i) );
+    		   x = ORIGIN_X + (int) ((j * SQRT_3 * size) + ((i%2) * size * SQRT_3 / 2));
+    		   text = i + ":" + j;
+    		   if (i == selectedRow && j == selectedColumn) {
+    	            drawHex(g, x, y, size, Color.BLACK, text);
+    	          } else {
+    	            drawHex(g, x, y, size, c, text);
+    	          }
+    	   }
+       }
+     
+}
+
+
 
     private void drawHex(Graphics g, int ox, int oy, int size, Color color, String text) {
       g.setColor(color);
-      /*int[] xPoints = new int [6];
-      int[] yPoints = new int [6];
-      for (int i = 0; i < 6; i++) {
-    	  double angle = (i + 0.5) * 2 * Math.PI / 6 ;
-    	  xPoints[i] = (int) (ox + SIZE * Math.cos(angle));
-    	  yPoints[i] = (int) (oy + SIZE * Math.sin(angle));
-    	  
-      }*/
-      int[] xPoints = {ox, (int) ((ox + (SQRT_3 / 2) * SIZE)), (int) ((ox + SQRT_3 / 2 * SIZE)), ox, (int) ((ox - SQRT_3 / 2 * SIZE)), (int) ((ox - (SQRT_3 / 2)* SIZE))};
-      int[] yPoints = {SIZE + oy, (int) (0.5 * SIZE + oy), (int) (oy - 0.5* SIZE), oy - 1* SIZE, (int) (oy - 0.5* SIZE), (int) (oy + 0.5* SIZE)};
-      
-      
+
+      int[] xPoints = {ox, (int) ((ox + (SQRT_3 / 2) * size)), (int) ((ox + SQRT_3 / 2 * size)), ox, (int) ((ox - SQRT_3 / 2 * size)), (int) ((ox - (SQRT_3 / 2)* size))};
+      int[] yPoints = {size + oy, (int) (0.5 * size + oy), (int) (oy - 0.5* size), oy - 1* size, (int) (oy - 0.5* size), (int) (oy + 0.5* size)};
+
+
       g.fillPolygon(xPoints, yPoints, 6);
       g.setColor(Color.BLACK);
       g.drawPolygon(xPoints, yPoints, 6);
+      // Calculate the position of the text within the hexagon
       g.setFont(g.getFont().deriveFont(24f));
-      //g.drawString(text, ox + SIZE + 2, oy + 2 + 2 * SIZE);
+      FontMetrics fontMetrics = g.getFontMetrics();
+      int textWidth = fontMetrics.stringWidth(text);
+      int textHeight = fontMetrics.getHeight();
+      int textX = ox - textWidth / 2;
+      int textY = oy + textHeight / 4;
+      // Draw the text
+      g.drawString(text, textX, textY);
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-      int x = e.getX() - ORIGIN_X;
-      int y = e.getY() - ORIGIN_Y;
-      if (x < 0 || y < 0 || x > numberOfBoxes  * SIZE * SQRT_3 || y > numberOfBoxes * 1.5 * SIZE) return;
-      int ii = (int) (y / (1.5 * SIZE));
-      int jj = (int) (x / ( SQRT_3 * SIZE));
-      int row = (int) Math.round(ii - 0.5 * ((jj % 2)));
-      int col = (int) Math.round(jj * 1);
-      selectedRow = row;
-      selectedColumn = col;
-      repaint();
-    }
+        int x = e.getX() - ORIGIN_X;
+        int y = e.getY() - ORIGIN_Y;
+        if (x < 0 || y < 0 || x > numberOfBoxes * 2 * size * SQRT_3 || y > numberOfBoxes * 3 * size) return;
+        int ii = y / (3 * size);
+        int jj = (int) Math.round(x / (SQRT_3 * size));
+
+        int row = (int) Math.round(ii - 0.5 * (jj % 2));
+        int col = (int) Math.round((2 * jj + row % 2) / 2);
+        selectedRow = row;
+        selectedColumn = col;
+        repaint();
+      }
 
     @Override
     public void mouseDragged(MouseEvent e) {}
