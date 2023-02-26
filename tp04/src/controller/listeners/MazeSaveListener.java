b63abac53e26eaf29a4584a06390af71c2393693
Arthur Gastineau <arthur@Arthur.local>
2023-02-26 14:22:11 +0100

    Forgot to add the listener in the commit

---
+++ tp04/src/controller/listeners/MazeSaveListener.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package controller.listeners;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+import controller.MazeActionListener;
+import controller.MazeController;
+
+/**
+ * @author arthur
+ *
+ */
+public class MazeSaveListener extends MazeActionListener {
+	public MazeSaveListener(MazeController mazeController) {
+		super(mazeController);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JFileChooser fileChooser = new JFileChooser("data/");
+		int returnVal = fileChooser.showSaveDialog(null);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File fileToSave = new File(fileChooser.getSelectedFile()+".maze");
+			mazeController.save(fileToSave.getAbsolutePath());
+		}
+	}
+}
